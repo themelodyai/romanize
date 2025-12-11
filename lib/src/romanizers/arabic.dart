03e6a0d792618b9333701b779cdbf705092d9511
Bruno D'Luka <brunodlukaa@gmail.com>
2025-12-10 23:44:59 -0300

    refactor: Remove dependency on arabic_roman_conv and implement custom transliteration map in ArabicRomanizer

--- lib/src/romanizers/arabic.dart
+++ lib/src/romanizers/arabic.dart
@@ -1,19 +1,84 @@
-import 'package:arabic_roman_conv/arabic_roman_conv.dart';
 import 'package:romanize/romanize.dart';
 
 /// A romanizer for Arabic script.
 ///
 /// Supports transliteration of Arabic characters into Latin script using
-/// the `arabic_roman_conv` package.
+/// a standard transliteration table based on ISO 233 and DIN 31635.
 class ArabicRomanizer extends Romanizer {
   const ArabicRomanizer() : super(language: 'arabic');
 
-  static final converter = ArabicRomanConv();
+  /// Transliteration map for Arabic to Latin characters.
+  ///
+  /// Based on ISO 233 and DIN 31635 standards, covering most common Arabic
+  /// characters and their romanized equivalents.
+  static const Map<String, String> _transliterationMap = {
+    // Basic Arabic alphabet
+    'ا': 'a', // Alef
+    'أ': 'a', // Alef with Hamza above
+    'إ': 'i', // Alef with Hamza below
+    'آ': 'ā', // Alef Madda
+    'ٱ': 'a', // Alef Wasla
+    'ب': 'b',
+    'ت': 't',
+    'ث': 'th',
+    'ج': 'j',
+    'ح': 'ḥ',
+    'خ': 'kh',
+    'د': 'd',
+    'ذ': 'dh',
+    'ر': 'r',
+    'ز': 'z',
+    'س': 's',
+    'ش': 'sh',
+    'ص': 'ṣ',
+    'ض': 'ḍ',
+    'ط': 'ṭ',
+    'ظ': 'ẓ',
+    'ع': 'ʿ',
+    'غ': 'gh',
+    'ف': 'f',
+    'ق': 'q',
+    'ك': 'k',
+    'ل': 'l',
+    'م': 'm',
+    'ن': 'n',
+    'ه': 'h',
+    'ة': 'h', // Teh Marbuta
+    'و': 'w',
+    'ؤ': 'u', // Waw with Hamza
+    'ي': 'y',
+    'ى': 'ā', // Alef Maksura
+    'ئ': 'i', // Yeh with Hamza
+    'ء': 'ʾ', // Hamza
+    // Arabic diacritics (usually omitted in romanization but included for completeness)
+    'َ': 'a', // Fatha
+    'ُ': 'u', // Damma
+    'ِ': 'i', // Kasra
+    'ً': 'an', // Tanwin Fath
+    'ٌ': 'un', // Tanwin Damm
+    'ٍ': 'in', // Tanwin Kasr
+    'ّ': '', // Shadda (gemination, usually handled contextually)
+    'ْ': '', // Sukun (no vowel, usually omitted)
+    'ٰ': 'ā', // Superscript Alef
+    // Arabic numbers
+    '٠': '0',
+    '١': '1',
+    '٢': '2',
+    '٣': '3',
+    '٤': '4',
+    '٥': '5',
+    '٦': '6',
+    '٧': '7',
+    '٨': '8',
+    '٩': '9',
+  };
 
   /// Converts a given Arabic string to its Romanized form.
   ///
-  /// This method uses the `arabic_roman_conv` package to transliterate
-  /// Arabic characters into their Latin equivalents.
+  /// This method transliterates Arabic characters into their Latin
+  /// equivalents using a standard transliteration table. Non-Arabic
+  /// characters (such as Latin letters, numbers, and punctuation) are
+  /// preserved as-is.
   ///
   /// Example:
   /// ```dart
@@ -23,7 +88,16 @@ class ArabicRomanizer extends Romanizer {
   /// ```
   @override
   String romanize(String input) {
-    return converter.romanized(input);
+    final buffer = StringBuffer();
+    for (final char in input.runes) {
+      final charString = String.fromCharCode(char);
+      if (_transliterationMap.containsKey(charString)) {
+        buffer.write(_transliterationMap[charString]);
+      } else {
+        buffer.write(charString);
+      }
+    }
+    return buffer.toString();
   }
 
   /// Validates if the input string contains Arabic characters.
